79dd80cead785a4f969c42af35d3a7787b589897
Renata Isis Rodrigues Sousa <81968935+Renata-Isis@users.noreply.github.com>
2022-11-24 12:50:09 -0300

    Update Loader.js

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -7,7 +7,7 @@ import loader from '../Loader/loader.gif';
 const Loader = () => {
   return (
     <C.LoaderContainer>
-    <img src={loader} alt="loader" />
+      <img src={loader} alt="loader" />
     </C.LoaderContainer>
   )
 }
